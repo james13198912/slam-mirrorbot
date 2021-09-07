8921bb96ec42b89ca414ad29c85c75bb6238172c
james13198912 <90074990+james13198912@users.noreply.github.com>
2021-09-07 12:48:35 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -587,9 +587,9 @@ class GoogleDriveHelper:
                     content += f'<b> | <a href="https://telegra.ph/{self.path[nxt_page]}">Next</a></b>'
                     nxt_page += 1
             Telegraph(access_token=telegraph_token).edit_page(path = self.path[prev_page],
-                                 title = 'Slam Mirrorbot Search',
-                                 author_name='Slam Mirrorbot',
-                                 author_url='https://github.com/SlamDevs/slam-mirrorbot',
+                                 title = 'Swifties World',
+                                 author_name='Swifties World',
+                                 author_url='https://t.me/Taylorswift13fanpage',
                                  html_content=content)
         return
 
